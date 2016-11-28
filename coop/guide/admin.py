97b77a30f08b02edeebf024e59023bf45e3b5136
jalibras <jamescruickshank71@gmail.com>
2016-11-28 10:07:24 +0000

    Add files via upload

---
+++ coop/guide/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from django.conf import settings
+from django.utils.html import format_html
+
+from guide.models import Problem
+
+
+# Register your models here.
+
+class ProblemAdmin(admin.ModelAdmin):
+    list_filter = ('grade','steepness')
+    list_display = ('grade','hold_colour','steepness','picture_1')
+
+    readonly_fields=('pictures',)
+    
+    def pictures(self,obj):
+        raw_list = [
+                obj.picture_1,
+                obj.picture_2,
+                obj.picture_3,
+                ]
+        pic_list = list(filter(lambda x:x.name!='',raw_list))
+
+        ht=""
+        for pic in pic_list:
+            try:
+                im_url = settings.MEDIA_URL+pic.name
+            except:
+                return 'upload a picture'
+            ht += "<p><img src='{url}' width='100%'/>".format(url=im_url)
+        return format_html(ht)
+
+
+
+admin.site.register(Problem,ProblemAdmin)
