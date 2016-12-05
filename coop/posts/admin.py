a13ed862d70bb80ce25c86b3c9a15b3e14e7451b
James Cruickshank <jamescruickshank71@gmail.com>
2016-12-05 19:45:19 +0000

    updated problem views

--- coop/posts/admin.py
+++ coop/posts/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 
-from posts.models import ProblemComment, VideoPost
 
 # Register your models here.
 
 
 
 
-
-admin.site.register(ProblemComment)
-admin.site.register(VideoPost)
