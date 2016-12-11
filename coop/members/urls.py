4c7bf93421a514722c15bcddf239ec23ba86ad13
James Cruickshank <jamescruickshank71@gmail.com>
2016-12-11 23:33:30 +0000

    auth

--- coop/members/urls.py
+++ coop/members/urls.py
@@ -6,13 +6,10 @@ from members import views
 # override the default login to implement remember me
 from django.contrib.auth.views import login
 
-def custom_login(*args,**kwargs):
-    return login(*args,**kwargs)
 
 
 
 urlpatterns = [
-    url(r'login',custom_login),
     url(r'^',include('django.contrib.auth.urls')),
     ]
 
