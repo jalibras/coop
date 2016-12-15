913aef58d66a0ebde56c69a1e597cea0c568c207
James Cruickshank <jamescruickshank71@gmail.com>
2016-12-15 08:22:39 +0000

    formsets??

--- coop/coop/settings.py
+++ coop/coop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z8opvv_h4s!t=m%@ip*reg@x9%_snhhy=229!gsi&6)4k0)qd='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jalibras.pythonanywhere.com']
+ALLOWED_HOSTS = ['jalibras.pythonanywhere.com','localhost']
 
 
 # Application definition
