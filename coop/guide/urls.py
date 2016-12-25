1b567555c314243a8ba973f11f3ffa341025127a
James Cruickshank <jamescruickshank71@gmail.com>
2016-12-25 00:04:40 +0000

    generic views

--- coop/guide/urls.py
+++ coop/guide/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls import url
 
 from guide import views
 
+from guide.views import ArtificialProblemList
+
+
+
 urlpatterns = [
     url(r'^index',views.index,name="index"),
     url(r'^area/(?P<areaid>\d+)',views.area,name="area"),
     url(r'^problem/(?P<id>[0-9]+)',views.problem,name="problem"),
     url(r'^submit',views.submitproblem,name="submit"),
+    url(r'^artificialproblems/',ArtificialProblemList.as_view()),
     ]
