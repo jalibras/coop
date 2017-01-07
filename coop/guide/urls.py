43324060fd69abe54b1d0ebb57b4686931e28ff4
James Cruickshank <jamescruickshank71@gmail.com>
2017-01-07 00:55:04 +0000

    update form

--- coop/guide/urls.py
+++ coop/guide/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     url(r'^problem/(?P<id>[0-9]+)',views.problem,name="problem"),
     url(r'^problem/toggle/(?P<userid>[0-9]+)/(?P<problemid>[0-9]+)',views.toggle_problem_status,name="toggle"),
     url(r'^problem/getstatus/(?P<userid>[0-9]+)/(?P<problemid>[0-9]+)',views.get_problem_status,name="getstatus"),
-    url(r'^submit',views.submitproblem,name="submit"),
+    url(r'^submit/(?P<problem_type>(natural)|(artificial))',views.submitproblem,name="submit"),
+    url(r'^update/(?P<problem_id>\d+)',views.submitproblem,name="update"),
     url(r'^artificialproblems/',ArtificialProblemList.as_view()),
     url(r'^artificialproblems/',ArtificialProblemList.as_view()),
     ]
