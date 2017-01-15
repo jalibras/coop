0f78ed3dd86017c95ee33d6a7deaad75d4fbfd43
James Cruickshank <galwayclimberscoop@gmail.com>
2017-01-15 22:32:55 +0000

    debug mode fix

--- coop/coop/settings.py
+++ coop/coop/settings.py
@@ -31,7 +31,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
-DEBUG = True
 if DJANGO_HOST=='dev':
     DEBUG = True
 
