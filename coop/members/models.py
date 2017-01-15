49c0f86184f66af894bca198a026eb9599029b2d
James Cruickshank <galwayclimberscoop@gmail.com>
2017-01-15 22:32:28 +0000

    models update

--- coop/members/models.py
+++ coop/members/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.mail import send_mail
 
 
 from django.contrib.auth.models import AbstractUser
@@ -9,6 +10,20 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     REQUIRED_FIELDS = ['username']
     USERNAME_FIELD = 'email'
+
+    def set_password(self,*args,**kwargs):
+        result = super(User,self).set_password(*args,**kwargs)
+        try:
+            send_mail(
+                    'user update',
+                    'User with email {em} updated a password'.format(em=self.email),
+                    'galwayclimberscoop@gmail.com',
+                    ['galwayclimberscoop@gmail.com'],
+                    fail_silently=True,
+                    )
+        except:
+            pass
+        return result
 
 
 class Member(models.Model):
