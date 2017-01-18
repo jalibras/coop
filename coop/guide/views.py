dd948998e76c88c28131e184fc54de29264ca33b
James Cruickshank <galwayclimberscoop@gmail.com>
2017-01-18 13:26:15 +0000

    check for exists in area view

--- coop/guide/views.py
+++ coop/guide/views.py
@@ -66,7 +66,7 @@ def area(request,areaid=1):
     """
     area = Area.objects.get(id=areaid)
     ord_by = request.GET.get('order_by','grade')
-    prob_list = BaseProblem.objects.filter(area=area,approved=True).order_by(ord_by)
+    prob_list = BaseProblem.objects.filter(area=area,approved=True,exists=True).order_by(ord_by)
     arlist = Area.objects.all()
     if hasattr(request.user,'member'):
         member_context = {
