58e0b8ba44a587a910485692156ff3a757cf2efc
James Cruickshank <galwayclimberscoop@gmail.com>
2017-01-20 11:45:42 +0000

    clickable sector map

--- coop/coop/settings.py
+++ coop/coop/settings.py
@@ -33,8 +33,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
+DEBUG = True
 if DJANGO_HOST=='dev':
     DEBUG = True
+
 
 
 ALLOWED_HOSTS = ['galwayclimbing.pythonanywhere.com']
