7c88929e0af6b36ab9158df2a4de5b0b833da33e
James Cruickshank <jamescruickshank71@gmail.com>
2017-01-28 23:42:17 +0000

    switch off f pagination is rest

--- coop/coop/settings.py
+++ coop/coop/settings.py
@@ -186,7 +186,7 @@ REST_FRAMEWORK = {
             'rest_framework.permissions.IsAuthenticatedOrReadOnly',
             #'rest_framework.permissions.IsAdminUser',
             ],
-        'PAGE_SIZE': 10,
+        'PAGE_SIZE': None,
         'DEFAULT_FILTER_BACKENDS':(
             'django_filters.rest_framework.DjangoFilterBackend',
             'rest_framework.filters.SearchFilter',
