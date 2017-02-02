5e9812509dffe71b821308f8b89819543555b232
James Cruickshank <jamescruickshank71@gmail.com>
2017-02-02 12:35:47 +0000

    added some tests of problem views

--- coop/guide/tests.py
+++ coop/guide/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
 
 from django.utils import timezone
 from guide.templatetags.problem_list import list_display
-from guide.models import ArtificialProblem,ProblemImage
+from guide.models import Area,Sector,BaseProblem,ArtificialProblem,ProblemImage
+from members.models import User,Member
 
 
 import random
@@ -10,53 +13,72 @@ import random
 # Create your tests here.
 
 
+# view tests
+
+class ProblemViewTest(TestCase):
+
+    fixtures = ['guide.json']
+
+    def setUp(self,*args,**kwargs):
+        user = User.objects.create_user('temp@gmail.com','temp@gmail.com','temp1234')
+        member = Member.objects.create(user=user)
+        super(ProblemViewTest,self).setUp(*args,**kwargs)
+
+    def test_problem_view_returns_200(self):
+        problems = BaseProblem.objects.all()
+        test_id = problems[0].id
+        response = self.client.get(reverse('guide:problem', args=[test_id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,'Problem')
+
+    def test_invalid_problem_id_returns_404(self):
+        c = 2*BaseProblem.objects.all().count()
+        p = BaseProblem.objects.filter(id=c)
+        while len(p) > 0:
+            c = 2*c
+            p = BaseProblem.objects.filter(id=c)
+        response = self.client.get(reverse('guide:problem', args=[c]))
+        self.assertEqual(response.status_code, 404)
+        
+
+
+    def test_artificial_problem_view_has_setter_field_but_no_firs_ascensionist(self):
+        problems = ArtificialProblem.objects.all().filter(exists=True)
+        test_id = problems[0].id
+        response = self.client.get(reverse('guide:problem', args=[test_id]))
+        self.assertContains(response,'etter')
+        self.assertNotContains(response,'scensionist')
+
+
+    def test_new_artificial_problem_save_works_for_logged_in_user(self):
+        area= Area.objects.all().filter(area_type='artificial')[0]
+
+        sector = Sector.objects.all().filter(area=area)[0]
+        u = User.objects.all()[0]
+        new_prob_data = {
+                'area':area.id,
+                'grade':'6A',
+                'sector':sector.id,
+                'description':'A test problem with a stupid description',
+                'setter':'Test Setter',
+                'date':'2017-01-01',
+# we need to include the following so that the image formset validates
+# i don't really understand this??
+                'problemimage_set-TOTAL_FORMS':'2',
+                'problemimage_set-INITIAL_FORMS':'0',
+                'problemimage_set-MAX_NUM_FORMS':'',
+                }
+        self.client.login(username='temp@gmail.com',password='temp1234')
+        response=self.client.get(reverse('guide:submit',args=['artificial']),new_prob_data)
+        response=self.client.post(reverse('guide:submit',args=['artificial']),new_prob_data)
+        self.assertEqual(response.status_code,200)
+# now check that the object is in the DB
+        p = ArtificialProblem.objects.get(
+                description=new_prob_data['description']
+                )
+
+
+
 # testing models
 
-
-class ProblemImageTest(TestCase):
-    def setUp(self):
-        ProblemImage.objects.create()
-
-    def test_base_problem_pictures_method(self):
-        pass
-
-
-
-class ArtificialProblemTest(TestCase):
-    def setUp(self):
-        ArtificialProblem.objects.create(
-                setter='unknown',
-                description='test problem 1',
-                holds = 'Mint',
-                date = timezone.now().date()
-                )
-            
-
-
-class ProblemImageTest(TestCase):
-    def setUp(self):
-        ProblemImage.objects.create(
-                problem = ArtificialProblem.objects.get(description='test problem 1'),
-                image_file=None
-                )
-
-    def test_base_problem_pictures_method(self):
-        pass
-
-
-test_artificial_problem = ArtificialProblem(
-        setter='unknown',
-        description='A test problem, Sit start.',
-        holds = 'Mint',
-        date = timezone.now().date()
-        )
-
-
-test_problems = [
-
-# test to make sure that my template tag works
-
-class ListDisplayTagTest(TestCase):
-    def test_list_display_not_NONE_for_date(self):
-        self.assertIsNot(list_display(test_artificial_problem,'date'),'NONE')
 
